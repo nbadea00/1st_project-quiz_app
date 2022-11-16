aca7af4d52a206b9c58277e7015b95dd63cf4f29
nbadea00 <nbadea61@gmail.com>
2022-11-16 19:05:00 +0100

    js-question

--- buildweek/result/script.js
+++ buildweek/result/script.js
@@ -10,5 +10,19 @@
     document.querySelector('#wrong .question').textContent =  (numDomande - voto)  + "/10" + " questions"
     document.querySelector('.circle').style.strokeDasharray = `${perGiuste}, 100`
     document.querySelector('.risult-text').textContent = perGiuste + "%";
-    document.querySelector('.wrong-text').textContent = perErrato + "%";
+    //document.querySelector('.wrong-text').textContent = perErrato + "%";
+
+    document.querySelector('.circle').onmousemove = function (){
+        document.querySelector('.pop-up-correct').style.visibility = 'visible'
+    }
+    document.querySelector('.circle').onmouseleave = function (){
+        document.querySelector('.pop-up-correct').style.visibility = 'hidden'
+    }
+
+    document.querySelector('.circle-bg').onmousemove = function (){
+        document.querySelector('.pop-up-wrong').style.visibility = 'visible'
+    }
+    document.querySelector('.circle-bg').onmouseleave = function (){
+        document.querySelector('.pop-up-wrong').style.visibility = 'hidden'
+    }
 }
